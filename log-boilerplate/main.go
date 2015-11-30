53b1cc4601b956bc834777025b7f80bacb99c880
Lucas Bremgartner <lucas@bremis.ch>
2015-11-30 21:10:00 +0100

    Make default logtarget platform independent

--- log-boilerplate/main.go
+++ log-boilerplate/main.go
@@ -9,7 +9,7 @@ import (
 
 var (
 	logLevel      = flag.Int("loglevel", 1, "0 = no output, 1 = standard log, 2 = verbose log")
-	logTarget     = flag.String("logtarget", "/dev/stdout", "log target")
+	logTarget     = flag.String("logtarget", "", "log target, default: stdout")
 	logPermission = flag.Int("logperm", 0600, "permission for log file, default in ocatal: 0600")
 
 	logTargetWriter io.Writer
@@ -31,36 +31,41 @@ func main() {
 	flag.Parse()
 
 	// Set logTarget
-	var logFile *os.File
-	fileMode := os.FileMode(*logPermission)
-	_, err = os.Stat(*logTarget)
-	if os.IsNotExist(err) {
-		logFile, err = os.Create(*logTarget)
-		if err != nil {
-			log.Fatalln("fatal: unable to create log target", err)
-		}
-
-		err = os.Chmod(*logTarget, fileMode)
-		if err != nil {
-			log.Fatalln("fatal: unable to set file permissions", err)
-		}
+	if *logTarget == "" {
+		logger = log.New(os.Stdout, "LOG   ", log.LstdFlags)
+		debugLogger = log.New(os.Stdout, "DEBUG ", log.LstdFlags)
 	} else {
-		logFile, err = os.OpenFile(*logTarget, os.O_WRONLY|os.O_APPEND, fileMode)
-		if err != nil {
-			log.Fatalln("fatal: unable to open log target", err)
+		var logFile *os.File
+		fileMode := os.FileMode(*logPermission)
+		_, err = os.Stat(*logTarget)
+		if os.IsNotExist(err) {
+			logFile, err = os.Create(*logTarget)
+			if err != nil {
+				log.Fatalln("fatal: unable to create log target", err)
+			}
+
+			err = os.Chmod(*logTarget, fileMode)
+			if err != nil {
+				log.Fatalln("fatal: unable to set file permissions", err)
+			}
+		} else {
+			logFile, err = os.OpenFile(*logTarget, os.O_WRONLY|os.O_APPEND, fileMode)
+			if err != nil {
+				log.Fatalln("fatal: unable to open log target", err)
+			}
 		}
-	}
-	defer logFile.Close()
-	logTargetWriter = io.Writer(logFile)
+		defer logFile.Close()
+		logTargetWriter = io.Writer(logFile)
 
-	// Check if logTarget is writable
-	_, err = logTargetWriter.Write([]byte(""))
-	if err != nil {
-		log.Fatalln("fatal: unable to write to log target")
-	}
+		// Check if logTarget is writable
+		_, err = logTargetWriter.Write([]byte(""))
+		if err != nil {
+			log.Fatalln("fatal: unable to write to log target")
+		}
 
-	logger = log.New(logTargetWriter, "LOG   ", log.LstdFlags)
-	debugLogger = log.New(logTargetWriter, "DEBUG ", log.LstdFlags)
+		logger = log.New(logTargetWriter, "LOG   ", log.LstdFlags)
+		debugLogger = log.New(logTargetWriter, "DEBUG ", log.LstdFlags)
+	}
 
 	// Set log-function based on log level
 	switch *logLevel {
