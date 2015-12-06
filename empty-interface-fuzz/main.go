d4b6ab445729d96df1fb9382c7d1cec547bb2ee4
Lucas Bremgartner <lucas@bremis.ch>
2015-12-06 14:42:23 +0100

    Improved empty interface example.

--- empty-interface-fuzz/main.go
+++ empty-interface-fuzz/main.go
@@ -4,11 +4,31 @@ import (
 	"fmt"
 )
 
-func foo(i interface{}) {
+func fooCorrect(i interface{}) {
+	x, ok := i.(int)
+	if ok {
+		fmt.Printf("int value is: %q\n", x)
+	} else {
+		fmt.Printf("value is not an int\n")
+	}
+}
+
+func fooCorrect2(i interface{}) {
+	switch x := i.(type) {
+	case int:
+		fmt.Println(x)
+	default:
+		fmt.Printf("value is not an int\n")
+	}
+}
+
+func fooBad(i interface{}) {
 	x := i.(int)
 	fmt.Println(x)
 }
 
 func main() {
-	foo("123")
+	fooCorrect("123")
+	fooCorrect2("123")
+	fooBad("123")
 }
