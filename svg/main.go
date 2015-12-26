5636983dce2bc76a8a25c3b578e1f28647bb9857
Lucas Bremgartner <lucas@bremis.ch>
2015-12-26 16:30:34 +0100

    svg with external file and css

--- svg/main.go
+++ svg/main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"github.com/ajstarks/svgo"
+	"fmt"
 	"os"
+
+	"github.com/ajstarks/svgo"
 )
 
 func main() {
 	width := 500
 	height := 500
-	canvas := svg.New(os.Stdout)
+	file, err := os.Create("./out.svg")
+	if err != nil {
+		panic(err)
+	}
+
+	canvas := svg.New(file)
 	canvas.Start(width, height)
+
+	canvas.Def()
+	fmt.Fprintf(canvas.Writer, "<style type=\"text/css\">\n<![CDATA[\n#test {fill:   #00cc00;}]]>\n</style>\n")
+	canvas.DefEnd()
+
+	canvas.Gid("test")
 	canvas.Circle(width/2, height/2, 100)
+	canvas.Gend()
+	canvas.Roundrect(100, 100, 100, 200, 0, 0, "fill:red")
 	canvas.Text(width/2, height/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
 	canvas.End()
 }
