afecd98d8b650c197cd354d2c28b04679cd74b2a
Lucas Bremgartner <lucas@bremis.ch>
2016-02-22 22:52:45 +0100

    Added copyright

--- gosnmp/main.go
+++ gosnmp/main.go
@@ -1,3 +1,7 @@
+// Copyright 2012 Andreas Louca. All rights reserved.
+// Use of this source code is goverend by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
