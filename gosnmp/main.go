cb59bf5f0c8d615738d739d06cdb73d474e81507
Lucas Bremgartner <lucas.bremgartner@united-security-providers.ch>
2016-04-08 08:07:27 +0200

    Changed gosnmp

--- gosnmp/main.go
+++ gosnmp/main.go
@@ -10,11 +10,11 @@ import (
 )
 
 func main() {
-	const cmdTimeout = 10
+	const cmdTimeout = 1
 	const cmdOid = ".1.3.6.1.4.1.12356.101.4.1.1" // Device Version
 	//const cmdOid = ".1.3.6.1.4.1.12356.101.4.1.4.0" // MemUsage
 
-	s, err := gosnmp.NewGoSNMP("10.168.1.1", "public", gosnmp.Version2c, cmdTimeout)
+	s, err := gosnmp.NewGoSNMP("10.10.120.1", "public", gosnmp.Version2c, cmdTimeout)
 	if err != nil {
 		fmt.Printf("Error creating SNMP instance: %s\n", err.Error())
 		return
@@ -24,26 +24,35 @@ func main() {
 
 	s.SetTimeout(cmdTimeout)
 	oid := cmdOid
-	for i := 0; i < 10; i++ {
-		fmt.Printf("Getting %s\n", oid)
-		resp, err := s.GetNext(oid)
-		if err != nil {
-			fmt.Printf("Error getting response: %s\n", err.Error())
-		} else {
-			for _, v := range resp.Variables {
-				fmt.Printf("%s -> ", v.Name)
-				switch v.Type {
-				case gosnmp.OctetString:
-					if s, ok := v.Value.(string); ok {
-						fmt.Printf("%s\n", s)
-					} else {
-						fmt.Printf("Response is not a string\n")
-					}
-				default:
-					fmt.Printf("Type: %s(%#x) - Value: %v\n", v.Type, int(v.Type), v.Value)
+	oids := []string{
+		".1.3.6.1.4.1.12356.101.4.1.1.0",
+		".1.3.6.1.4.1.12356.101.4.1.2.0",
+		".1.3.6.1.4.1.12356.101.4.1.3.0",
+		".1.3.6.1.4.1.12356.101.4.1.4.0",
+		".1.3.6.1.4.1.12356.101.4.1.5.0",
+	}
+	_ = oids
+	//for i := 0; i < 10; i++ {
+	fmt.Printf("Getting %s\n", oid)
+	//resp, err := s.GetMulti(oids) // Max 5
+	resp, err := s.GetBulk(0, 50, ".1.3.6.1.4.1.12356.101.4.1.1")
+	if err != nil {
+		fmt.Printf("Error getting response: %s\n", err.Error())
+	} else {
+		for _, v := range resp.Variables {
+			fmt.Printf("%s -> ", v.Name)
+			switch v.Type {
+			case gosnmp.OctetString:
+				if s, ok := v.Value.(string); ok {
+					fmt.Printf("%s\n", s)
+				} else {
+					fmt.Printf("Response is not a string\n")
 				}
-				oid = v.Name
+			default:
+				fmt.Printf("Type: %s(%#x) - Value: %v\n", v.Type, int(v.Type), v.Value)
 			}
+			oid = v.Name
 		}
 	}
+	//}
 }
